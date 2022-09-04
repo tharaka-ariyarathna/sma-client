b4db70f9d666c19c41cd00a4f8d5121b2228dd77
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-09-05 02:42:39 +0530

    Changed the url to new one

--- src/api/UploadApi.js
+++ src/api/UploadApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios' ;
 
-const API = axios.create({baseURL: "http://localhost:5000"}) ;
+const API = axios.create({baseURL: "https://sma-mern.herokuapp.com"}) ;
 
 export const uploadImage = (data) => API.post("/upload/", data);
 
