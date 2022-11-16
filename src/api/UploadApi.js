2d1c6209e10869e13db68c560a90d03e88f6041a
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-11-16 14:02:45 +0530

    Changed api url

--- src/api/UploadApi.js
+++ src/api/UploadApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios' ;
 
-const API = axios.create({baseURL: "https://sma-mern.herokuapp.com"}) ;
+const API = axios.create({baseURL: "http://localhost:5000"}) ;
 
 export const uploadImage = (data) => API.post("/upload/", data);
 
