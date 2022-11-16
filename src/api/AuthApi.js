2b85d61301a600b7d9446ed77abd05e954f925f2
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-11-16 14:01:03 +0530

    Update AuthApi.js
    Changed the api url

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios' ;
 
-const API = axios.create({baseURL: "https://sma-mern.herokuapp.com"}) ;
+const API = axios.create({baseURL: "http://localhost:5000"}) ;
 
 export const login = (formData) => API.post('/auth/login', formData) ;
 
