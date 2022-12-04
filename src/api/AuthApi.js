d77f4705a17574475987474d6a6bd47b4bf043eb
Taraka <tharakaariyarathna04@gmail.com>
2022-12-04 21:43:19 +0530

    Checking

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -5,3 +5,5 @@ const API = axios.create({baseURL: "https://sma-server.vercel.app/"}) ;
 export const login = (formData) => API.post('/auth/login', formData) ;
 
 export const signUp = (formData) => API.post('/auth/register', formData) ;
+
+//This is for checking
