f3618eccb6f1434aabd05a64e542672522543d26
Taraka <tharakaariyarathna04@gmail.com>
2022-12-22 00:13:02 +0530

    refactor

--- src/api/UploadApi.js
+++ src/api/UploadApi.js
@@ -5,3 +5,6 @@ const API = axios.create({baseURL: "https://sma-server.vercel.app/"}) ;
 export const uploadImage = (data) => API.post("/upload/", data);
 
 export const uploadPost = (data) => API.post('/post/', data) ;
+
+
+//Checking
