c0c81caed1b047be0df7ca46207cade3a98ff779
Taraka <tharakaariyarathna04@gmail.com>
2023-01-06 13:38:14 +0530

    Changed the UI of InfoCard

--- src/components/infoCard/InfoCard.js
+++ src/components/infoCard/InfoCard.js
@@ -1,72 +1,92 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import {useParams} from 'react-router-dom' ;
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { UilPen } from "@iconscout/react-unicons";
 import ProfileModal from "../profileModal/ProfileModal";
-import * as UserApi from '../../api/UserApi' ;
+import * as UserApi from "../../api/UserApi";
 import "./InfoCard.css";
 
 const InfoCard = () => {
   const [modalOpened, setModalOpened] = useState(false);
-  const [profileUser, setProfileUser] = useState({name:"Tharaka"}) ;
+  const [profileUser, setProfileUser] = useState({});
 
-  const dispatch = useDispatch() ;
-  const params = useParams() ;
+  const dispatch = useDispatch();
+  const params = useParams();
 
   const userId = params.id;
 
-  const {user} = useSelector(state => state.Authreducer.authData.data) ;
+  const { user } = useSelector((state) => state.Authreducer.authData.data);
 
   useEffect(() => {
-    const fetchProfileUser = async() => {
-      if(userId === user._id){
-        setProfileUser(user) ;
-      }else{
-        const profileUser = await UserApi.getUser(userId) ;
-        setProfileUser(profileUser.data) ;
+    const fetchProfileUser = async () => {
+      if (userId === user._id) {
+        setProfileUser(user);
+      } else {
+        const profileUser = await UserApi.getUser(userId);
+        setProfileUser(profileUser.data);
       }
-    }
+    };
 
-    fetchProfileUser() ;
-  }, [user])
+    fetchProfileUser();
+  }, [user]);
 
   return (
     <div className="infocard">
       <div className="infoHead">
-        <h4>Your Info</h4>
-        <div>
-          <UilPen
-            width="2rem"
-            height="1.2rem"
-            onClick={() => setModalOpened(true)}
-          />
-          <ProfileModal
-            modalOpened={modalOpened}
-            setModalOpened={setModalOpened}
-          />
+        <h4>Profile Info</h4>
+        {profileUser._id === user._id ? (
+          <div>
+            <UilPen
+              width="2rem"
+              height="1.2rem"
+              onClick={() => setModalOpened(true)}
+            />
+            <ProfileModal
+              modalOpened={modalOpened}
+              setModalOpened={setModalOpened}
+            />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      {profileUser.relationShip ? (
+        <div className="info">
+          <span>
+            <b>Status</b>
+          </span>
+          <span>&nbsp;&nbsp; {profileUser.relationShip}</span>
         </div>
-      </div>
-      <div className="info">
-        <span>
-          <b>Status</b>
-        </span>
-        <span>&nbsp;&nbsp; Single</span>
-      </div>
-      <div className="info">
-        <span>
-          <b>Lives in</b>
-        </span>
-        <span>&nbsp;&nbsp; Dublin</span>
-      </div>
-      <div className="info">
-        <span>
-          <b>Works at</b>
-        </span>
-        <span>&nbsp;&nbsp; Dublin harbour</span>
-      </div>
-      <button className="button lo-button">Log Out</button>
+      ) : (
+        ""
+      )}
+      {profileUser.livesIn ? (
+        <div className="info">
+          <span>
+            <b>Lives in</b>
+          </span>
+          <span>&nbsp;&nbsp; {profileUser.livesIn}</span>
+        </div>
+      ) : (
+        ""
+      )}
+      {profileUser.worksAt ? (
+        <div className="info">
+          <span>
+            <b>Works at</b>
+          </span>
+          <span>&nbsp;&nbsp; Dublin harbour</span>
+        </div>
+      ) : (
+        ""
+      )}
+      {profileUser._id === user._id ? (
+        <button className="button lo-button">Log Out</button>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
