f24cef3ad6db992001ef000348d6eb86016a361b
Taraka <tharakaariyarathna04@gmail.com>
2023-01-07 15:49:47 +0530

    Created functions to get all users

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -5,3 +5,5 @@ const API = axios.create({ baseURL: "http://localhost:5000/" });
 export const getUser = (userId) => API.get(`user/${userId}`) ;
 
 export const updateUser = (userId, data) => API.put(`user/${userId}`, data) ;
+
+export const getAllUsers = () => API.get(`user/`)
--- src/components/followersCard/FollowersCard.js
+++ src/components/followersCard/FollowersCard.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { followers } from '../../data/FolloersData';
 import User from "../user/user";
-import img1 from "../../img/avatar1.png";
+import { useSelector } from "react-redux";
 import "./FollowersCard.css";
+import { getAllUsers } from "../../api/UserApi";
 
 const FollowersCard = () => {
+  const {user} = useSelector((state) => state.Authreducer.authData.data);
+  const [data, setData] = useState({}) ;
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const {data} = await getAllUsers() ;
+      setData(data.data) ;
+    }
+    fetchData() ;
+  },[])
+
   return (
     <div className="followersCard">
       <h3>People You May Know</h3>
