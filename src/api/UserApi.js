7a7ebfeb36533fca3ce20a14723fb861d1a120a9
Taraka <tharakaariyarathna04@gmail.com>
2023-01-09 16:22:33 +0530

    Changed userApi

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -4,7 +4,7 @@ const API = axios.create({ baseURL: "http://localhost:5000/" });
 
 API.interceptors.request.use(req => {
     if(localStorage.getItem("profile")){
-        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).data.token}`
+        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`
     }
     return req ;
 })
