34865f788acf9e8ed368d10dac55753ed7204502
Taraka <tharakaariyarathna04@gmail.com>
2023-01-11 20:25:04 +0530

    Created shareSide component

---
+++ src/components/searchSide/searchSide.js
@@ -0,0 +1,11 @@
+import React from 'react' ;
+
+const SearchSide = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default SearchSide ;
--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,11 +1,22 @@
-import react from 'react' ;
+import React from "react";
+import PostSide from "../../components/postside/PostSide";
+import RightSide from "../../components/rightSide/RightSide";
+import SearchSide from "../../components/searchSide/searchSide";
+import Profileside from "../../components/profileside/Profileside";
+import "./search.css";
 
 const search = () => {
-    return(
-        <div className='search'>
+  return (
+    <div className="search">
+      <Profileside location={"homePage"} />
+      <PostSide location={"homePage"} />
+      <RightSide />
+    </div>
+  );
+};
 
-        </div>
-    )
-}
+export default search;
 
-export default seach ;
+/*(<SearchSide />
+      <PostSide location={"homePage"} />
+      <RightSide />)*/
